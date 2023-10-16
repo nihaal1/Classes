e6bf139b63e1bd0147f254a8c9697673a42a7f0d
Nihaal <nzaheer@iastate.edu>
2023-10-16 16:25:24 -0500

    cpre 431 added files

---
+++ cpre489/Lab3/file.c
@@ -0,0 +1,15 @@
+//include libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+// ./file <SOURCE IP> <SOURCE PORT> <DESTINATION IP> <DESTINATION PORT> <LOSS RATE>
+int main(int argc, char ** argv){
+
+
+
+
+}

1e21d608ee8afad4d83d37ed8cb412d8dbfc7b3b
Zaheer <nzaheer@iastate.edu>
2024-09-13 14:30:22 -0500

    Completed Lab2

--- cpre388/Lab2 - PocketCalculator/PocketCalculator_skeleton/PocketCalculator_skeleton/app/src/main/java/edu/iastate/netid/pocketcalculator/MainActivity.java
+++ cpre388/Lab2 - PocketCalculator/PocketCalculator_skeleton/PocketCalculator_skeleton/app/src/main/java/edu/iastate/netid/pocketcalculator/MainActivity.java
@@ -92,7 +92,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    public void onDotClicked(View view) {
+        try {
+            mCalculationStream.inputDigit(CalculationStream.Digit.DECIMAL);
+        } finally {
+            updateCalculatorDisplay();
+        }
+    }
 
     public void onOneClicked(View view) {
         try {

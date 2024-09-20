00c0c7aa7c054d3fd6f57b0189a0da0390ac9bee
Zaheer <nzaheer@iastate.edu>
2024-09-20 12:44:25 -0500

    made modifications for the StopwatchViewModel.java

--- cpre388/Lab3 - Stopwatch/StopwatchSample/app/src/main/java/com/example/stopwatchsample/StopwatchViewModel.java
+++ cpre388/Lab3 - Stopwatch/StopwatchSample/app/src/main/java/com/example/stopwatchsample/StopwatchViewModel.java
@@ -1,13 +1,31 @@
 package com.example.stopwatchsample;
 
+import android.os.Handler;
+import android.os.SystemClock;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
 public class StopwatchViewModel extends ViewModel {
 
     // Variable to track elapsed time
     private long elapsedTime = 0L;
+    private boolean isRunning = false;
+    private final Handler handler = new Handler();
+    private  final MutableLiveData<Long> elapsedTimeLiveData = new MutableLiveData<>();
 
     // TODO: Add methods to start, stop, and reset the stopwatch
-
-    public long getElapsedTime() {
-        return elapsedTime;
+    public StopwatchViewModel(){
+        elapsedTimeLiveData.setValue(elapsedTime);
     }
+
+//    public long getElapsedTime() {
+//        return elapsedTime;
+//    }
+
+    public LiveData<Long> getElapsedTime(){
+        return elapsedTimeLiveData;
+    }
+
 }
